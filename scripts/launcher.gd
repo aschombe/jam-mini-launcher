1c67fe182a8bcb1ddad68a4c20e7f4341250f83b
andrew <aschombe@stevens.edu>
2024-12-05 13:40:15 -0500

    Finished functionality, next design

--- scripts/launcher.gd
+++ scripts/launcher.gd
@@ -1,11 +1,141 @@
 extends Control
 
+const JSON_DIR : String = "/home/andrew/Documents/projects/jam-mini-launcher/jsons/"
+const GAME_DIR : String = "/home/andrew/Documents/projects/jam-mini-launcher/games/"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var game_grid: GridContainer = $game_scroller/game_grid
 
+var jsons : DirAccess 
+var json_name : String
+var game_jsons : PackedStringArray
+var games : DirAccess
+var folder_name : String
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var game_folder : String
+var game_exec_path : String
+var game_thumbnail_path : String
+var thumbnail_texture : Texture2D
+var resized_thumbnail : Image
+var game_folder_contents : PackedStringArray
+
+var game_button : Button
+
+var json_string : String 
+var json_dict : Dictionary
+@onready var info_panel: Control = $info_panel
+@onready var game_title: Label = $info_panel/game_title
+@onready var author: Label = $info_panel/author
+@onready var genres: Label = $info_panel/genres
+@onready var description: Label = $info_panel/description
+@onready var play_button: Button = $info_panel/play_button
+
+var info_name
+var info_genres
+var info_author
+var info_description
+
+var game_running : bool = false
+var running_pid : int = -1
+var connected = false
+
 func _process(_delta: float) -> void:
-	pass
+	# Prevents more than one game from being lanuched at a time
+	if game_running and OS.is_process_running(running_pid) == false:
+		game_running = false
+
+func _ready() -> void:
+	info_panel.visible = false
+	
+	jsons = DirAccess.open(JSON_DIR)
+	if jsons:
+		jsons.list_dir_begin()
+		json_name = jsons.get_next()
+		while json_name != "":
+			json_name = json_name.get_basename()
+			game_jsons.append(json_name)
+			json_name = jsons.get_next()
+	game_jsons.sort()
+			
+	games = DirAccess.open(GAME_DIR)
+	if games:
+		games.list_dir_begin()
+		folder_name = games.get_next()
+		while folder_name != "":
+			if game_jsons.has(folder_name):
+				# Create the file paths
+				game_folder = GAME_DIR + folder_name
+				game_folder_contents = DirAccess.get_files_at(game_folder)
+				game_thumbnail_path = game_folder + "/" + folder_name + ".png"
+				game_exec_path = game_folder + "/" + folder_name + ".x86_64"
+				
+				# Instantiate and add the button
+				game_button = Button.new()
+				game_button.set_meta("exec_path", game_exec_path)  # Store the exec path
+				#game_button.set_meta("game_name", game_folder) # store game name
+				
+				# Open the corresponding JSON and attach all the relevant information to the button as metadata
+				var json_file = FileAccess.open(JSON_DIR + folder_name + ".json", FileAccess.READ)
+				if json_file:
+					json_string = json_file.get_as_text()  # Read the entire JSON file as a single string
+					var json = JSON.new()
+					var parse_result = json.parse(json_string)
+					if parse_result != OK:
+						print("JSON Parse Error: ", json.get_error_message(), " in ", json_string)
+					else:
+						var data = json.get_data()
+						if data.has("name"):
+							info_name = data["name"]
+						if data.has("author"):
+							info_author = data["author"]
+						if data.has("description"):
+							info_description = data["description"]
+						if data.has("genres"):
+							info_genres = data["genres"]
+
+						# Attach metadata to the button
+						game_button.set_meta("name", info_name)
+						game_button.set_meta("author", info_author)
+						game_button.set_meta("description", info_description)
+						game_button.set_meta("genres", info_genres)
+				else:
+					print("Failed to open JSON file: ", folder_name + ".json")
+				
+				add_child(game_button)
+				thumbnail_texture = load(game_thumbnail_path)
+				resized_thumbnail = thumbnail_texture.get_image()
+				resized_thumbnail.resize(235, 187)
+				game_button.icon = ImageTexture.create_from_image(resized_thumbnail)
+				game_button.reparent(game_grid)
+				
+				# Connect the button signal, and bind the game_button as an input to it
+				game_button.connect("pressed", update_info_panel.bind(game_button))
+				
+			folder_name = games.get_next()
+
+func update_info_panel(button: Button) -> void:
+	# Populate the labels with the appropriate text
+	game_title.text = button.get_meta("name")
+	author.text = button.get_meta("author")
+	description.text = button.get_meta("description")
+	genres.text = button.get_meta("genres")
+
+	# Connect the button signal, and bind the game_button as an input to it
+	if connected:
+		play_button.disconnect("pressed", _on_game_button_pressed)
+		connected = false
+	play_button.connect("pressed", _on_game_button_pressed.bind(button))
+	connected = true
+	info_panel.visible = true
+
+# Callback for when a button is pressed
+func _on_game_button_pressed(button: Button) -> void:
+	if game_running:
+		return
+		
+	var exec_path = button.get_meta("exec_path")  # Retrieve the exec path from metadata
+	if exec_path:
+		#var result = OS.execute(exec_path, [])
+		running_pid = OS.create_process(exec_path, [])
+		game_running = true
+	else:
+		print("No exec path found for button: ", button.name)
